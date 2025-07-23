3abee8de25be5cbe98eb1c1f891861bdc0fe1d28
Anderson <anderson.oliveira.venturini@gmail.com>
2025-07-23 18:01:54 -0300

    ajustes interface projeto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,33 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-               <div class="relative flex-1 rounded-lg px-6 py-4 bg-zinc-50 dark:bg-zinc-700   ">
-                    <div class="[:where(&amp;)]:text-sm [:where(&amp;)]:text-zinc-500 [:where(&amp;)]:dark:text-white/70" data-flux-text="">Estoque</div>
-                    <div class="font-medium [:where(&amp;)]:text-zinc-800 [:where(&amp;)]:dark:text-white text-2xl [&amp;:has(+[data-flux-subheading])]:mb-2 [[data-flux-subheading]+&amp;]:mt-2 mb-2 mt-2" data-flux-heading="">$38,393.12</div>
-
-                    <div class="flex items-center gap-1 font-medium text-sm  text-green-600 dark:text-green-400 ">
-                        <svg class="shrink-0 [:where(&amp;)]:size-4" data-flux-icon="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-  <path fill-rule="evenodd" d="M9.808 4.057a.75.75 0 0 1 .92-.527l3.116.849a.75.75 0 0 1 .528.915l-.823 3.121a.75.75 0 0 1-1.45-.382l.337-1.281a23.484 23.484 0 0 0-3.609 3.056.75.75 0 0 1-1.07.01L6 8.06l-3.72 3.72a.75.75 0 1 1-1.06-1.061l4.25-4.25a.75.75 0 0 1 1.06 0l1.756 1.755a25.015 25.015 0 0 1 3.508-2.85l-1.46-.398a.75.75 0 0 1-.526-.92Z" clip-rule="evenodd"></path>
-</svg>
-
-         16.2%
-                    </div>
-
-                    <div class="absolute top-0 right-0 pr-2 pt-2">
-                        <button type="button" class="relative items-center font-medium justify-center gap-2 whitespace-nowrap disabled:opacity-75 dark:disabled:opacity-75 disabled:cursor-default disabled:pointer-events-none h-8 text-sm rounded-md w-8 inline-flex  bg-transparent hover:bg-zinc-800/5 dark:hover:bg-white/15 text-zinc-500 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-white" data-flux-button="data-flux-button">
-
-    </button>
-                    </div>
+            <div
+                class="relative h-40 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 flex items-center justify-center text-center">
+                <div>
+                    <flux:heading>
+                        Clientes <flux:badge color="lime" inset="top bottom">Cadastrados</flux:badge>
+                    </flux:heading>
+                    <flux:text class="mt-2">Todos os clientes.</flux:text>
                 </div>
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+
+            <div
+                class="relative h-40 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 flex items-center justify-center text-center">
+                <div>
+                    <flux:heading>
+                        Produtos <flux:badge color="lime" inset="top bottom">Cadastrados</flux:badge>
+                    </flux:heading>
+                    <flux:text class="mt-2">Todos os produtos.</flux:text>
+                </div>
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div
+                class="relative h-40 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 flex items-center justify-center text-center">
+                <div>
+                    <flux:heading>
+                        Vendas <flux:badge color="lime" inset="top bottom">Cadastrados</flux:badge>
+                    </flux:heading>
+                    <flux:text class="mt-2">Todos as vendas.</flux:text>
+                </div>
             </div>
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
