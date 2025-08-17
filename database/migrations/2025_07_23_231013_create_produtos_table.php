7b71c553b8e01e541e3aa736cc1cf2a99144247a
Anderson <anderson.oliveira.venturini@gmail.com>
2025-08-17 14:01:13 -0300

    sku produto

--- database/migrations/2025_07_23_231013_create_produtos_table.php
+++ database/migrations/2025_07_23_231013_create_produtos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('sku')->unique()->comment('Stock Keeping Unit - Código único para identificar o produto em estoque.');   
             $table->string('nome');
             $table->string('tipo_produto_sped')->nullable()->comment('Tipo do produto conforme a classificação do SPED - Indica a categoria do produto segundo as normas do Sistema Público de Escrituração Digital (SPED).');
             $table->string('ncm')->nullable()->comment('Nomenclatura Comum do Mercosul - Código utilizado para identificar a natureza de um produto no comércio internacional.');
