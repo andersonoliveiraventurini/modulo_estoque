6a3e389a86dab704f665b4f1e8a5b52b7e00a6d3
Anderson <anderson.oliveira.venturini@gmail.com>
2025-08-17 00:35:31 -0300

    ajustes estrutura produto

--- database/migrations/2025_07_23_231013_create_produtos_table.php
+++ database/migrations/2025_07_23_231013_create_produtos_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
+            $table->string('tipo_produto_sped')->nullable()->comment('Tipo do produto conforme a classificação do SPED - Indica a categoria do produto segundo as normas do Sistema Público de Escrituração Digital (SPED).');
+            $table->string('ncm')->nullable()->comment('Nomenclatura Comum do Mercosul - Código utilizado para identificar a natureza de um produto no comércio internacional.');
             $table->string('descricao')->nullable();
             $table->string('tamanho')->nullable();
             $table->timestamps();
