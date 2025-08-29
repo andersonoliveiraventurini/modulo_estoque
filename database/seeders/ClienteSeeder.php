e2f9f29131c57218f9ec84f6566bc1f8bf15307a
Anderson <anderson.oliveira.venturini@gmail.com>
2025-08-29 20:00:37 -0300

    carga inicial de dados dos clientes

--- database/seeders/ClienteSeeder.php
+++ database/seeders/ClienteSeeder.php
@@ -12,6 +12,63 @@ class ClienteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // carga de clientes inicial
+        /**
+    <Cell><Data ss:Type="String">numero</Data></Cell>
+    <Cell><Data ss:Type="String">nome</Data></Cell>
+    <Cell><Data ss:Type="String">endereço</Data></Cell>
+    <Cell><Data ss:Type="String">bairro</Data></Cell>
+    <Cell><Data ss:Type="String">cidade</Data></Cell>
+    <Cell><Data ss:Type="String">cep</Data></Cell>
+    <Cell><Data ss:Type="String">telefone_res</Data></Cell>
+    <Cell><Data ss:Type="String">telefone_cial</Data></Cell>
+    <Cell><Data ss:Type="String">celular</Data></Cell>
+    <Cell><Data ss:Type="String">referencias</Data></Cell>
+    <Cell><Data ss:Type="String">cpf</Data></Cell>
+    <Cell><Data ss:Type="String">rg</Data></Cell>
+    <Cell><Data ss:Type="String">cnpj</Data></Cell>
+    <Cell><Data ss:Type="String">usuario</Data></Cell>
+    <Cell><Data ss:Type="String">cliente</Data></Cell>
+    <Cell><Data ss:Type="String">fornecedor</Data></Cell>
+    <Cell><Data ss:Type="String">nascimento</Data></Cell>
+    <Cell><Data ss:Type="String">email</Data></Cell>
+    <Cell><Data ss:Type="String">contato</Data></Cell>
+    <Cell><Data ss:Type="String">vencimento</Data></Cell>
+    <Cell><Data ss:Type="String">valor</Data></Cell>
+    <Cell><Data ss:Type="String">carta</Data></Cell>
+    <Cell><Data ss:Type="String">seproc</Data></Cell>
+    <Cell><Data ss:Type="String">idcarta</Data></Cell>
+    <Cell><Data ss:Type="String">enviado</Data></Cell>
+    <Cell><Data ss:Type="String">bloqueio</Data></Cell>
+    <Cell><Data ss:Type="String">ref_cial</Data></Cell>
+    <Cell><Data ss:Type="String">emi_rg</Data></Cell>
+    <Cell><Data ss:Type="String">filiação</Data></Cell>
+    <Cell><Data ss:Type="String">funcionario</Data></Cell>
+    <Cell><Data ss:Type="String">empresa</Data></Cell>
+    <Cell><Data ss:Type="String">fantasia</Data></Cell>
+    <Cell><Data ss:Type="String">codmun</Data></Cell>
+    <Cell><Data ss:Type="String">casa</Data></Cell>
+    <Cell><Data ss:Type="String">UF</Data></Cell>
+    <Cell><Data ss:Type="String">avisar</Data></Cell>
+    <Cell><Data ss:Type="String">complemento</Data></Cell>
+    <Cell><Data ss:Type="String">limite</Data></Cell>
+    <Cell><Data ss:Type="String">vendedor</Data></Cell>
+    <Cell><Data ss:Type="String">compl</Data></Cell>
+    <Cell><Data ss:Type="String">momento</Data></Cell>
+    <Cell><Data ss:Type="String">referencias2</Data></Cell>
+    <Cell><Data ss:Type="String">ultima</Data></Cell>
+    <Cell><Data ss:Type="String">referencias3</Data></Cell>
+    <Cell><Data ss:Type="String">entrega</Data></Cell>
+    <Cell><Data ss:Type="String">arquivo</Data></Cell>
+    <Cell><Data ss:Type="String">externo</Data></Cell>
+    <Cell><Data ss:Type="String">carteira</Data></Cell>
+    <Cell><Data ss:Type="String">cheque</Data></Cell>
+    <Cell><Data ss:Type="String">boleto</Data></Cell>
+    <Cell><Data ss:Type="String">cadastro</Data></Cell>
+    <Cell><Data ss:Type="String">venc_limite</Data></Cell>
+    <Cell><Data ss:Type="String">desconto</Data></Cell>
+    <Cell><Data ss:Type="String">cf</Data></Cell>
+    <Cell><Data ss:Type="String">tratamento</Data></Cell>
+     */
     }
 }
