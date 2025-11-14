3939b183d44a301a4988c0f89c46025a02a5d5dc
Anderson de Oliveira Venturini <45111567+andersonoliveiraventurini@users.noreply.github.com>
2025-11-14 11:52:12 -0300

    ajustes seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Anderson',
             'email' => 'anderson@gmail.com',
             'password' => Hash::make('12345'),
